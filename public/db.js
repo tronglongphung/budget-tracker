a2e88e16029e8dab7e36ae2a5fbcda711fb6ef73
tronglongphung <tronglongphung@gmail.com>
2021-07-13 22:10:54 +0930

    adding dexie

---
+++ public/db.js
@@ -0,0 +1,29 @@
+const db = new Dexie("transaction_database");
+db.version(1).stores({
+  pendingTransaction: "name,value",
+});
+
+const saveRecord = (data) => {
+  db.pendingTransaction
+    .put({ name: data.name, value: data.value })
+    .then(function () {
+      console.log("Successfully saved", db);
+    })
+    .catch(function (error) {
+      alert("Ooops: " + error);
+    });
+};
+
+if (navigator.onLine) {
+  db.pendingTransaction.toArray().then((data) => {
+    console.log(data);
+    fetch("/api/transaction/bulk", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+    }).then(() => db.pendingTransaction.clear());
+  });
+}
