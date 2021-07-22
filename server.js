c3978273318f4851d9c6a272f93d470a74b1cad0
tronglongphung <tronglongphung@gmail.com>
2021-07-22 18:46:30 +0930

    adding database location

--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://tronglongphung:Hungonly030@cluster0.7irz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
