64961af196eb44cf271de3fd8d1304a8602e9060
tronglongphung <tronglongphung@gmail.com>
2021-07-22 18:54:51 +0930

    relocate location

--- server.js
+++ server.js
@@ -15,14 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  "mongodb+srv://tronglongphung:Hungonly030@cluster0.7irz3.mongodb.net/budget-tracker?retryWrites=true&w=majority",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 // routes
 app.use(require("./routes/api.js"));
